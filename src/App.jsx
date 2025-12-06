808cf4ab2ce05f1a456bcc3bc787f261680cb9f5
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-12-06 09:44:29 -0700

    implemented routing and a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Homepage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
