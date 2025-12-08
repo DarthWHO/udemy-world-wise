d5234d3a2075d62b34bd3a380740b233257b2684
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-12-08 10:42:45 -0700

    implemented context api for cities data

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,11 @@ import styles from "../styles/CountryList.module.css";
 import Spinner from "./Spinner";
 import CountryItem from "./CountryItem";
 import Message from "./Message";
+import { useCities } from "../hooks/useCities";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
   if (cities.length === 0)
     return (
